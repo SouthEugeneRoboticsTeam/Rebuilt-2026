46add292c6b0739097c5e329dffac6ffad5d661e
Drive 1/Theodore <benji.urbancic@gmail.com>
2026-01-27 17:37:56 -0800

    Started Indexer

--- src/main/java/org/sert2521/rebuilt2026/Constants.kt
+++ src/main/java/org/sert2521/rebuilt2026/Constants.kt
@@ -1,14 +1,39 @@
 package org.sert2521.rebuilt2026
 
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine
 import org.sert2521.rebuilt2026.util.HSGoal
+import yams.gearing.GearBox
+import yams.gearing.MechanismGearing
 
 object GrintakeConstants {
 
 }
 
 object IndexerConstants {
+    val IndexerGearing = MechanismGearing(
+        GearBox.fromReductionStages(
+            1.0
+        )
+    )
+    val kickerGearing = MechanismGearing(
+        GearBox.fromReductionStages(
+            1.0
+        )
+    )
+    const val MAIN_DEFAULT = 0.0
+    const val KICKER_DEFAULT = 0.0
 
+    const val MAIN_INDEXING = 0.0
+    const val KICKER_INDEXING = 0.0
+
+    const val MAIN_KICKING = 0.0
+    const val KICKER_KICKING = 0.0
+    const val KICK_TIME = 0.0
+
+    const val MAIN_REVERSE = 0.0
+    const val KICKER_REVERSE = 0.0
 }
+
 
 object HoodedShooterConstants {
 
--- src/main/java/org/sert2521/rebuilt2026/subsystems/Indexer.kt
+++ src/main/java/org/sert2521/rebuilt2026/subsystems/Indexer.kt
@@ -1,7 +1,43 @@
 package org.sert2521.rebuilt2026.subsystems
 
+import com.revrobotics.spark.SparkLowLevel
+import com.revrobotics.spark.SparkMax
+import edu.wpi.first.units.Units.Amps
+import edu.wpi.first.wpilibj.motorcontrol.Spark
 import edu.wpi.first.wpilibj2.command.SubsystemBase
+import org.sert2521.rebuilt2026.ElectronicIDs
+import org.sert2521.rebuilt2026.IndexerConstants
+import yams.motorcontrollers.SmartMotorController
+import yams.motorcontrollers.SmartMotorControllerConfig
+import yams.motorcontrollers.local.SparkWrapper
+import yams.telemetry.MechanismTelemetry
 
 object Indexer : SubsystemBase() {
+    private val indexerMotor = SparkMax(ElectronicIDs)
 
+    private val IndexerMotorConfig = SmartMotorControllerConfig(this)
+        .withGearing(IndexerConstants.IndexerGearing)
+        .withIdleMode(SmartMotorControllerConfig.MotorMode.BREAK)
+        .withTelemetry("Indexer Motor", SmartMotorControllerConfig.TelemetryVerbosity.LOW)
+        .withStatorCurrentLimit(Amps.of(0.0))
+        .withMotorInverted(false)
+        .withControlMode(SmartMotorControllerConfig.ControlMode.OPEN_LOOP)
+
+    private val indexerSMC = SparkWrapper(indexerMotor, DCMotor.Neo(0), IndexerMotorConfig)
+
+    private val kickerMotor = SparkMax(ElectronicIDs.KICKER_MOTOR_ID, SparkLowLevel.MotorType)
+
+    private val kickerMotorConfig = SmartMotorControllerConfig(this)
+        .withGearing(IndexerConstants.kickerGearing)
+        .withIdleMode(SmartMotorControllerConfig.MotorMode.BRAKE)
+        .withTelemetry("Kicker Motor", SmartMotorControllerConfig.TelemetryVerbosity.LOW)
+        .withStatorCurrentLimit(Amps.of(0.0))
+        .withMotorInverted(false)
+        .withControlMode(SmartMotorControllerConfig.ControlMode.OPEN_LOOP)
+
+    private val kickerSMC = SparkWrapper(kickerMotor, DCmotor.getNEO(0), kickerMotorConfig)
+
+    private val telemetry = MechanismTelemetry()
+
+    init
 }
