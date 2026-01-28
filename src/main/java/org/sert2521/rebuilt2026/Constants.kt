410bee89ea9d6eb5aab2ee5b31fbc93411a50a07
leolajeanfix-cell <leolajeanfix@gmail.com>
2026-01-27 17:56:45 -0800

    Finished Intake Subsystem

--- src/main/java/org/sert2521/rebuilt2026/Constants.kt
+++ src/main/java/org/sert2521/rebuilt2026/Constants.kt
@@ -1,9 +1,37 @@
 package org.sert2521.rebuilt2026
 
+import edu.wpi.first.units.Units.Degrees
+import edu.wpi.first.units.Units.KilogramSquareMeters
+import edu.wpi.first.units.Units.Rotations
 import org.sert2521.rebuilt2026.util.HSGoal
+import yams.gearing.GearBox
+import yams.gearing.MechanismGearing
+
+object ElectronicIDs {
+    const val GRINTAKE_ROLLER_MOTOR_ID = 1
+    const val GRINTAKE_WRIST_MOTOR_ID = 2
+}
 
 object GrintakeConstants {
+    val rollerGearing = MechanismGearing(
+        GearBox.fromReductionStages(
+            //add reduction stages
+        )
+    )
+    val wristGearing = MechanismGearing(
+        GearBox.fromReductionStages(
+            //add reduction stages
+        )
+    )
 
+    val stowPosition = Degrees.of(0.0)
+    val intakePosition = Degrees.of(0.0)
+
+    val intakeSpeed = 0.2
+    val reverseSpeed = -0.2
+
+    val hardMin = Rotations.of(0.0)
+    val hardMax = Rotations.of(0.0)
 }
 
 object IndexerConstants {
@@ -15,9 +43,5 @@ object HoodedShooterConstants {
 }
 
 object OtherConstants {
-
-}
-
-object ElectronicIDs {
 
 }
--- src/main/java/org/sert2521/rebuilt2026/subsystems/Grintake.kt
+++ src/main/java/org/sert2521/rebuilt2026/subsystems/Grintake.kt
@@ -1,7 +1,96 @@
 package org.sert2521.rebuilt2026.subsystems
 
+import com.revrobotics.spark.SparkLowLevel
+import com.revrobotics.spark.SparkMax
+import edu.wpi.first.hal.simulation.AnalogGyroDataJNI.setAngle
+import edu.wpi.first.math.system.plant.DCMotor
+import edu.wpi.first.units.Units.Amps
+import edu.wpi.first.units.Units.KilogramSquareMeters
+import edu.wpi.first.units.Units.Rotations
+import edu.wpi.first.wpilibj.DutyCycle
+import edu.wpi.first.wpilibj.motorcontrol.Spark
+import edu.wpi.first.wpilibj2.command.Command
+import edu.wpi.first.wpilibj2.command.Commands
 import edu.wpi.first.wpilibj2.command.SubsystemBase
+import org.sert2521.rebuilt2026.ElectronicIDs
+import org.sert2521.rebuilt2026.GrintakeConstants
+import yams.gearing.MechanismGearing
+import yams.mechanisms.config.ArmConfig
+import yams.mechanisms.positional.Arm
+import yams.motorcontrollers.SmartMotorControllerConfig
+import yams.motorcontrollers.local.SparkWrapper
+import yams.telemetry.MechanismTelemetry
 
 object Grintake : SubsystemBase() {
+    private val rollerMotor = SparkMax(ElectronicIDs.GRINTAKE_ROLLER_MOTOR_ID, SparkLowLevel.MotorType.kBrushless)
+    private val wristMotor = SparkMax(ElectronicIDs.GRINTAKE_WRIST_MOTOR_ID, SparkLowLevel.MotorType.kBrushless)
 
+    private val rollerMotorConfig = SmartMotorControllerConfig(this)
+        .withGearing(GrintakeConstants.rollerGearing)
+        .withMotorInverted(false)
+        .withIdleMode(SmartMotorControllerConfig.MotorMode.BRAKE)
+        .withTelemetry("Grintake Roller Motor", SmartMotorControllerConfig.TelemetryVerbosity.LOW)
+        .withStatorCurrentLimit(Amps.of(40.0))
+
+    private val wristMotorConfig = SmartMotorControllerConfig(this)
+        .withTelemetry("Grintake Wrist Motor", SmartMotorControllerConfig.TelemetryVerbosity.LOW)
+        .withMotorInverted(false)
+        .withStatorCurrentLimit(Amps.of(40.0))
+        .withIdleMode(SmartMotorControllerConfig.MotorMode.BRAKE)
+        .withGearing(GrintakeConstants.wristGearing)
+
+    private val smc = SparkWrapper(wristMotor, DCMotor.getNEO(1), wristMotorConfig)
+
+    private val wristConfig = ArmConfig(smc)
+        .withHardLimit(GrintakeConstants.hardMin, GrintakeConstants.hardMax)
+        .withTelemetry("Wrist", SmartMotorControllerConfig.TelemetryVerbosity.LOW)
+        .withStartingPosition(Rotations.of(0.0))
+
+    private val wrist = Arm(wristConfig)
+
+    private val RollersSMC = SparkWrapper(
+        rollerMotor, DCMotor.getNEO(1),
+        rollerMotorConfig
+    )
+
+    private val WristSMC = SparkWrapper(
+        wristMotor, DCMotor.getNEO(1),
+        wristMotorConfig
+    )
+
+    private val telemetry = MechanismTelemetry()
+
+    init{
+        defaultCommand = stow()
+        telemetry.setupTelemetry("Grintake", RollersSMC)
+        telemetry.setupTelemetry("Grintake", WristSMC)
+    }
+
+    override fun periodic() {
+        RollersSMC.updateTelemetry()
+        WristSMC.updateTelemetry()
+    }
+
+    private fun setRollerMotor(dutyCycle: Double) {
+        RollersSMC.dutyCycle = dutyCycle
+    }
+
+    fun stow(): Command {
+        return runOnce{
+            wrist.setAngle(GrintakeConstants.stowPosition)
+        }
+    }
+
+    fun intake(): Command {
+        return runOnce {
+            wrist.setAngle(GrintakeConstants.intakePosition)
+            setRollerMotor(GrintakeConstants.intakeSpeed)
+        }
+    }
+
+    fun reverse(): Command {
+        return runOnce {
+            setRollerMotor(GrintakeConstants.reverseSpeed)
+        }
+    }
 }
