bd634978dff61a3c48ca7820f747786de4fb74c0
Me 2.0 <benji.urbancic@gmail.com>
2026-01-29 16:34:09 -0800

    refactor

--- src/main/java/org/sert2521/rebuilt2026/subsystems/Grintake.kt
+++ src/main/java/org/sert2521/rebuilt2026/subsystems/Grintake.kt
@@ -2,22 +2,14 @@ package org.sert2521.rebuilt2026.subsystems
 
 import com.revrobotics.spark.SparkLowLevel
 import com.revrobotics.spark.SparkMax
-import edu.wpi.first.hal.simulation.AnalogGyroDataJNI.setAngle
 import edu.wpi.first.math.system.plant.DCMotor
 import edu.wpi.first.units.Units.Amps
-import edu.wpi.first.units.Units.KilogramSquareMeters
-import edu.wpi.first.units.Units.Rotations
 import edu.wpi.first.units.measure.Angle
-import edu.wpi.first.wpilibj.DutyCycle
-import edu.wpi.first.wpilibj.motorcontrol.Spark
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.Commands
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import org.sert2521.rebuilt2026.ElectronicIDs
 import org.sert2521.rebuilt2026.GrintakeConstants
-import yams.gearing.MechanismGearing
-import yams.mechanisms.config.ArmConfig
-import yams.mechanisms.positional.Arm
 import yams.motorcontrollers.SmartMotorControllerConfig
 import yams.motorcontrollers.local.SparkWrapper
 import yams.telemetry.MechanismTelemetry
@@ -30,17 +22,17 @@ object Grintake : SubsystemBase() {
         .withGearing(GrintakeConstants.rollerGearing)
         .withMotorInverted(false)
         .withIdleMode(SmartMotorControllerConfig.MotorMode.BRAKE)
-        .withTelemetry("Grintake Roller Motor", SmartMotorControllerConfig.TelemetryVerbosity.LOW)
+        .withTelemetry("Roller Motor", SmartMotorControllerConfig.TelemetryVerbosity.LOW)
         .withStatorCurrentLimit(Amps.of(40.0))
 
     private val wristMotorConfig = SmartMotorControllerConfig(this)
-        .withTelemetry("Grintake Wrist Motor", SmartMotorControllerConfig.TelemetryVerbosity.LOW)
+        .withTelemetry("Wrist Motor", SmartMotorControllerConfig.TelemetryVerbosity.LOW)
         .withMotorInverted(false)
         .withStatorCurrentLimit(Amps.of(40.0))
         .withIdleMode(SmartMotorControllerConfig.MotorMode.BRAKE)
         .withGearing(GrintakeConstants.wristGearing)
 
-    private val rollersSMC = SparkWrapper(
+    private val rollerSMC = SparkWrapper(
         rollerMotor, DCMotor.getNEO(1),
         rollerMotorConfig
     )
@@ -54,17 +46,17 @@ object Grintake : SubsystemBase() {
 
     init{
         defaultCommand = stow()
-        telemetry.setupTelemetry("Grintake", rollersSMC)
+        telemetry.setupTelemetry("Grintake", rollerSMC)
         telemetry.setupTelemetry("Grintake", wristSMC)
     }
 
     override fun periodic() {
-        rollersSMC.updateTelemetry()
+        rollerSMC.updateTelemetry()
         wristSMC.updateTelemetry()
     }
 
     private fun setRollerMotor(dutyCycle: Double) {
-        rollersSMC.dutyCycle = dutyCycle
+        rollerSMC.dutyCycle = dutyCycle
     }
 
     private fun setWristMotorAngle(angle: Angle){
