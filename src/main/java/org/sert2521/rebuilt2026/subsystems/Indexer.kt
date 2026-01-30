c4996741e78538945345b8bf340c8955e22e004f
AnnabelleW <2strawberrytoast@gmail.com>
2026-01-29 16:57:38 -0800

    Finished Indexer

--- src/main/java/org/sert2521/rebuilt2026/subsystems/Indexer.kt
+++ src/main/java/org/sert2521/rebuilt2026/subsystems/Indexer.kt
@@ -4,15 +4,15 @@ import com.revrobotics.spark.SparkLowLevel
 import com.revrobotics.spark.SparkMax
 import edu.wpi.first.math.system.plant.DCMotor
 import edu.wpi.first.units.Units.Amps
+import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import org.sert2521.rebuilt2026.ElectronicIDs
 import org.sert2521.rebuilt2026.IndexerConstants
 import yams.motorcontrollers.SmartMotorControllerConfig
 import yams.motorcontrollers.local.SparkWrapper
-import yams.telemetry.MechanismTelemetry
 
-object Indexer : SubsystemBase() {
-    private val indexerMotor = SparkMax(ElectronicIDs, SparkLowLevel.MotorType.kBrushless)
+object IndexerSubsystem : SubsystemBase() {
+    private val indexerMotor = SparkMax(ElectronicIDs.INDEXER_MOTOR_ID, SparkLowLevel.MotorType.kBrushless)
 
     private val indexerMotorConfig = SmartMotorControllerConfig(this)
         .withGearing(IndexerConstants.IndexerGearing)
@@ -36,9 +36,34 @@ object Indexer : SubsystemBase() {
 
     private val kickerSMC = SparkWrapper(kickerMotor, DCMotor.getNEO(1), kickerMotorConfig)
 
-    private val telemetry = MechanismTelemetry()
+    override fun periodic() {
+        indexerSMC.updateTelemetry()
+        kickerSMC.updateTelemetry()
+    }
 
-    init {
+    override fun simulationPeriodic() {
+        indexerSMC.simIterate()
+        kickerSMC.simIterate()
+    }
+    fun setIndexerMotor (dutyCycle: Double){
+      indexerSMC.dutyCycle = dutyCycle
+    }
 
+    fun setKickerMotor (dutyCycle: Double) {
+        kickerSMC.dutyCycle = dutyCycle
+    }
+
+    fun index(): Command {
+    return runOnce {
+        setIndexerMotor(0.0)
+        setKickerMotor(0.0)
+        }
+    }
+
+    fun kick(): Command{
+        return runOnce {
+            setIndexerMotor(0.0)
+            setKickerMotor(0.0)
+        }
     }
 }
